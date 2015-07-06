b9d96c189a8e9e44ae0831d22ba69013ffd68268
Skyler DeGrote <skyler.m.degrote@outlook.com>
2015-07-06 10:39:56 -0500

    Resume ReReChallenge
    click button to reveal resume

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+
+
+
+    $(".resumepart").hide();
+    $("#hide").click(function(){
+        $(".cover-part").slideUp(1000);
+         $(".resumepart").show(1000);
+    });
+    
+
+
+
+
+
+
+});
